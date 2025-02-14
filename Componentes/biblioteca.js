d56290cf83806b7a658e76a0ccdd44c4005d1371
Matheus612-coder <matheusserpa2000@hotmail.com>
2025-02-14 09:24:31 -0400

    Adicionada a pasta com os desenho padrão para as Telas do adm.

---
+++ Componentes/biblioteca.js
@@ -0,0 +1,30 @@
+let botao = document.getElementsByClassName("hamburguer")
+let menu = document.getElementById("menu")
+let corpo = document.getElementsByTagName("body")
+
+let menuAberto = false
+
+botao[0].addEventListener("click", function(){
+    
+    //Chamar o menu para o botão e definir sua posição com position 
+    //absolute para aparecer abaixo do menu. Linhas 15 e 16
+
+    menu.style.position="absolute" 
+    menu.style.bottom="50px" 
+
+    if(menuAberto==false){
+        menuAberto=true
+        menu.style.display="block"
+    }
+
+    else{
+        menuAberto=false
+        menu.style.display="none"
+    }
+
+})
+
+
+
+
+
