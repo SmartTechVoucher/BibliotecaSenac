24e11416b5f976020deb365249bd530098984265
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-03-25 16:53:58 -0400

    subindo imagens e corrigido caminho do css

--- projeto/src/views/usuario/telaFiltroLivros.php
+++ projeto/src/views/usuario/telaFiltroLivros.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../../public/css/telaFiltroLivros.css">
+    <link rel="stylesheet" href="../../../public/css/telaFiltroLivros.css">
 </head>
 
 <body>
@@ -284,7 +284,12 @@
             </div>
         </div>
     </div>
-    <script src="../../public/js/telaFiltroLivros.js"></script>
+
+    
+    
+    <script src="../../public/js/telaFiltroLivros.js">
+        <div>
+    </script>
 </body>
 
 </html>
