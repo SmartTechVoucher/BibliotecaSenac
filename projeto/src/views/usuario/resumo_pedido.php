684b29a156730ad5517dea8abe24766ed584d7d1
Vitória Silva <vitoria54099746@aluno.ms.senac.br>
2025-04-01 14:52:59 -0400

    a

---
+++ projeto/src/views/usuario/resumo_pedido.php
@@ -0,0 +1,33 @@
+<?php
+
+$livro = [
+    "titulo" => "Simpósio do Barreado",
+    "img" => "../../../public/assets/img/Simposio.png",
+    "desc" => "O livro, o autor aborda a pergunta chave: \"Afinal, o barreado nasceu em Paranaguá, Antonina ou Morretes?\". Esta pergunta é a razão do \"Simpósio do Barreado\". O livro mostra as origens e a receita do mais tradicional prato culinário do Paraná. Realizado ficticiamente em Porto de Cima, o simpósio reuniu especialistas de ontem e de hoje, daqui e de muitos lugares, em acaloradas discussões que naturalmente, terminaram em confraternização em volta da mesa. O barreado tem indicação geográfica (IG) na categoria \"indicação de procedência\" desde 2022 e é"
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pedido</title>
+    <link rel="stylesheet" href="../../../public/css/livro_info.css">
+    
+</head>
+<body>
+    <!-- header  -->
+    <?php   
+        include "../../../public/components/header/header.php";
+    ?>
+
+    
+    <!-- conteudo da pagina -->
+   
+    <?php   
+        include "../../../public/components/footer/footer.php";
+    ?>
+</body>
+</html>
