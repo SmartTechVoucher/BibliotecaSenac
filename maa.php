833af77457520222241738b231d1e11e922113e0
Vitor H. <74419765+vitocrack@users.noreply.github.com>
2025-04-15 14:23:03 -0400

    Adicionada parte da tela de usuários cadastrados.
    falta header e footer

---
+++ maa.php
@@ -0,0 +1,89 @@
+<h1>histórico de empréstimos</h1>
+        
+    </head>
+    <body>
+        
+        <table>
+            <thead>
+                <tr>
+                    <th>Status</th>
+                    <th>Exemplar</th>
+                    <th>Leitor</th>
+                    <th>Data</th>
+                    <th>Prazo</th>
+                    <th>Devolução</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody id="tabela-corpo">
+                <tr>
+                    <td>devolvido</td>
+                    <td>402864</td>
+                    <td>victor hugo</td>
+                    <td>20/02/25</td>
+                    <td>30/02/25</td>
+                    <td>19/02/2025</td>
+                    <td>
+                        <button type img="s" class="edit" onclick="editar(this)">Editar</button>
+                        <button class="delete" onclick="excluir(this)">Excluir</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        
+    </tr>
+</thead>
+<tbody id="tabela-corpo">
+    <table>
+    <tr>
+        <td>Emprestado</td>
+        <td>785930</td>
+        <td>lulu oliveira</td>
+        <td>20/02/2025</td>
+        <td>03/03</td>
+        <td>30/02/2025</td>
+        <td>
+            <button class="edit" onclick="editar(this)">Editar</button>
+            <button class="delete" onclick="excluir(this)">Excluir</button>
+        </td>
+    </tr>
+</tbody>
+</table>
+</tr>
+</thead>
+<tbody id="tabela-corpo">
+    <table>
+    <tr>
+        <td>Emprestado</td>
+        <td>80909099</td>
+        <td>João</td>
+        <td>20/02/2025</td>
+        <td>10 dias</td>
+        <td>30/02/2025</td>
+        <td>
+            <button class="edit" onclick="editar(this)">Editar</button>
+            <button class="delete" onclick="excluir(this)">Excluir</button>
+        </td>
+    </tr>
+</tbody>
+</table>
+</tr>
+</thead>
+<tbody id="tabela-corpo">
+    <table>
+    <tr>
+        <td>Emprestado</td>
+        <td>944512</td>
+        <td>João</td>
+        <td>20/02/2025</td>
+        <td>04/</td>
+        <td>30/02/2025</td>
+        <td>
+            <button class="edit" onclick="editar(this)">Editar</button>
+            <button class="delete" onclick="excluir(this)">Excluir</button>
+        </td>
+    </tr>
+</tbody>
+</table>
+</tr>
+</thead>
