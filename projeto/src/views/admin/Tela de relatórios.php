2e1df9509cf74fc2aed82f536d8efbc2e12f140a
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-05-06 15:21:30 -0400

    Correção dos arquivos css

--- projeto/src/views/admin/Tela de relatórios.php
+++ projeto/src/views/admin/Tela de relatórios.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de relatórios</title>
-    <link rel="stylesheet" href="../../../public/css/Tela de relatórios.css">
+    <link rel="stylesheet" href="../../../public/css/admin/telaRelatorios.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
