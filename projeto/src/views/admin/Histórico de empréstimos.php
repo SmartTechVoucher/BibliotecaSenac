a1e1d0b741ce7949c7425aa1bb35ea823caf7b82
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-05-06 16:38:26 -0400

    Correção: corrigindo rotas de arquivos css

--- projeto/src/views/admin/Histórico de empréstimos.php
+++ projeto/src/views/admin/Histórico de empréstimos.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Tela de usuários cadastrados</title>
-        <link rel="stylesheet" href="../../../public/css/admin/Tela de usuários cadastrados.css">
+        <link rel="stylesheet" href="../../../public/css/admin/telaUsuariosCadastrados.css">
     </head>
 
  <body>
