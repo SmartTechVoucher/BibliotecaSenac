807535a6e4dd49d55524e969c90648b27385f2a7
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-05-06 15:21:30 -0400

    Correção dos arquivos css

--- projeto/src/views/admin/Tela de cadastro de livros.php
+++ projeto/src/views/admin/Tela de cadastro de livros.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de cadastro de livros</title>
-    <link rel="stylesheet" href="../../../public/css/Tela de cadastro de livros.css">
+    <link rel="stylesheet" href="../../../public/css/admin/telaCadastroLivros.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- projeto/src/views/admin/Tela inicial do adm.php
+++ projeto/src/views/admin/Tela inicial do adm.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela inicial do adm</title>
-    <link rel="stylesheet" href="../../../public/css/Tela inicial do adm.css">
+    <link rel="stylesheet" href="../../../public/css/admin/telaInicialAdm.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
