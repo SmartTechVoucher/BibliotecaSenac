22ab78d26759bb68b256fc4b4a7082da5605f00e
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-05-09 17:14:14 -0400

    refatorado footer e arrumado os caminhos de alguns arquivos de style

--- projeto/public/components/footer/footer.php
+++ projeto/public/components/footer/footer.php
@@ -14,6 +14,7 @@ require "../../../config/constantes.php"
     <link rel="stylesheet" href="<?php echo $URLBASE?>/public/css/components/footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet">
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
@@ -26,8 +27,8 @@ require "../../../config/constantes.php"
                 <img src="<?php echo $URLBASE?>/public/assets/img/logosenac.svg" fill="white" alt="Logo Senac" id="logo-senac">
             </div>
             
-            <div class="livro">
-                <img src="/public/livro.svg" alt="Livro icon">
+            <div class="livro-brilhante">
+                <img src="../../../public/assets/img/livro-brilhante.svg " alt="Livro icon">
                 <p>Senac MS Copyright © <br>
                     2024. Todos os Direitos Reservados</p>
             </div>
