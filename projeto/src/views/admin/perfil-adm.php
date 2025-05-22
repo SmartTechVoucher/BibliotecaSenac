fde9e6ca31f9f41242a6099d9768d98031c48b7e
bia-sx <loreslainebianca@gmail.com>
2025-05-22 15:22:57 -0400

    Implementado tela de perfil do adm

---
+++ projeto/src/views/admin/perfil-adm.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de relatórios</title>
+    <link rel="stylesheet" href="./style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montaga&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    
+ <!--Cabeçalho--> <!--Cabeçalho--> <!--Cabeçalho-->   
+    <div class="conteiner">     
+        
+        <div class="menuAdm">
+            <img src="../Icones/Menu adm.png" alt="" class="hamburguer">
+            <img src="../Icones/Logo senac hub.png" alt="Imagem do logo" class="logoSenacHub">
+        </div>
+            
+        <div class="titulo"> 
+            <h2>Biblioteca Senac</h2> 
+            <h2 class="senac">Senac Mato Grosso do Sul</h2> 
+        </div>
+
+        <div class="icone">
+            <img src="../Icones/Icon perfil.png" alt="Ícone de pessoa">
+        </div>    
+  
+    </div>
+
+ <!--Corpo--> <!--Corpo--> <!--Corpo--> <!--Corpo--> <!--Corpo-->   
+
+     <div class="conteiner3">
+
+        <div class="superior">
+            <img src="./Ícones/Superior esquerdo.png" alt="Superior esquerdo" class="esquerdo">
+        </div>
+
+        <div class="superior">
+            <img src="./Ícones/Superior direito.png" alt="Superior direito" class="direito">
+        </div>
+
+    </div>
+
+
+    <div class="quadradoBranco">
+        
+        <form class="quadradoCinza">
+
+    Início / Perfil / Minha conta
+
+<h1>luciano (786543)</h1>
+
+  <form class="quadradoCinza">
+
+    <div class="linhaCima">
+
+        <div class="campo">
+
+            <p>*perfil de acesso:</p>
+            <input type="text" name="nome">
+            
+        </div>
+
+        <div class="form-container">
+            <p>*email</p>
+            <input type="email" id="email" name="email" />
+          
+            
+</div>
+<div class="campoSenha">
+            <p>*senha</p>
+            <input type="password" id="senha" name="senha">
+            </div>
+
+</div>
+<div class="linhaBaixo">
+    <div class="campo">
+        <p>*telefone</p>
+        <input type="tel" id="tel" name="tel">
+        &nbsp;
+    </div>
+    <br><button class="edit-btn">editar informação</button><br/>
+    <div class="esqueciSenha">
+    <br><button class="esqueciSenha">Esqueci minha senha</button></br>
+</div>
+</div>
+            </div>
+        </form>
+    </div>
+    
+    <div class="inferior">
+
+        <div class="direito">
+            <img src="./Ícones/Inferior direito.png" alt="">
+        </div>     
+
+    </div>
+            
+   <!--Rodapé-->  <!--Rodapé-->  <!--Rodapé-->  <!--Rodapé-->  <!--Rodapé-->  <!--Rodapé-->  <!--Rodapé-->  <!--Rodapé-->  <!--Rodapé-->  <!--Rodapé-->  <!--Rodapé-->  <!--Rodapé-->
+
+    <div class="rodape">
+
+        <div class="logoFecomercio"><img src="../Icones/Fecomercio.png" alt="" class="Logo" ></div>
+        
+        <div><img src="./Ícones/Livro.png" alt="" class="Libro">
+            <div class="copyright">Senac MS Copyright © <br></div>
+            <div class="todos"> 2024. Todos os Direitos Reservados</div>   
+        </div>
+        
+        <div class="faleConosco">
+
+            <div><h4>Fale Conosco</h4>
+                <div>Central de atendimento (67) 3312-6260</div>  
+            </div>
+           
+        </div>
+        
+        <div class="emailAtendimento"><h4>Email: <br> atendimento@ms.senac.br <br> Sugestões,
+        dúvidas <br> elogios ou críticas</h4></div>
+        
+        <div class="siga-nos"><h4>Siga-nos</h4>
+        
+            <img src="./Ícones/Facebook.png" alt="">
+            <img src="./Ícones/Instagram Circle.png" alt="">
+            <img src="./Ícones/LinkedIn.png" alt="">
+            <img src="./Ícones/Twitter Squared.png" alt="">
+            <br>
+            <img src="./Ícones/WhatsApp.png" alt="">
+            <img src="./Ícones/YouTube.png" alt="">                 
+        
+        </div>
+
+    </div>
+
+    
+    <!-- <div><a href="./Conexões entre telas/biblioteca.html">Cadastrar livros</a>Cadastrar livros</div>--> 
+    <!--Refazer o menu com ligações entre telas, criar uma pasta com os ícones para a tela de cadastro de livros-->
+
+    <script src="./biblioteca.js"></script>
+
+</body>
+</html>
