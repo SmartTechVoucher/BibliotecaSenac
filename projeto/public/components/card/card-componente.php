b7d27cfffa9d8f1bce33d0aa9932fcfad20ba39f
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-06-05 17:15:42 -0400

    correcao card

--- projeto/public/components/card/card-componente.php
+++ projeto/public/components/card/card-componente.php
@@ -5,27 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../../../public/css/components/card_livros.css">
+    <link rel="stylesheet" href="../../../public/css/components/card-livros.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 
     <div class="card-livro">
-        <img 
+        <img
             src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1714763387i/212703311.jpg"
             alt="Capa do livro"
             class="capa-livro">
 
         <div class="container-info">
+            <h3 class="titulo-livro">Guia do Mestre - D&D</h3>
             <div>
-                <h3 class="titulo-livro">Guia do Mestre - D&D</h3>
                 <p class="autor-livro">Autor:</p>
                 <p class="autor-nome">(Nome do autor)</p>
             </div>
-            
+
             <div class="status-container">
                 <p class="status-livro">Disponível</p>
                 <button class="btn-ver-livro">Ver livro</button>
             </div>
         </div>
     </div>
-</body>
-</html>
+    </body>
 
+</html>
