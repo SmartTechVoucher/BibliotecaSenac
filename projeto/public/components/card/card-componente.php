ad7db72b4b7823672708c85c538d5341fc869587
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-06-06 16:08:08 -0400

    removendo head

--- projeto/public/components/card/card-componente.php
+++ projeto/public/components/card/card-componente.php
@@ -1,32 +1,19 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<div class="card-livro">
+    <img
+        src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1714763387i/212703311.jpg"
+        alt="Capa do livro"
+        class="capa-livro">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../../../public/css/components/card-livros.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <div class="container-info">
+        <h3 class="titulo-livro">Guia do Mestre - D&D</h3>
+        <div>
+            <p class="autor-livro">Autor:</p>
+            <p class="autor-nome">(Nome do autor)</p>
+        </div>
 
-    <div class="card-livro">
-        <img
-            src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1714763387i/212703311.jpg"
-            alt="Capa do livro"
-            class="capa-livro">
-
-        <div class="container-info">
-            <h3 class="titulo-livro">Guia do Mestre - D&D</h3>
-            <div>
-                <p class="autor-livro">Autor:</p>
-                <p class="autor-nome">(Nome do autor)</p>
-            </div>
-
-            <div class="status-container">
-                <p class="status-livro">Disponível</p>
-                <button class="btn-ver-livro">Ver livro</button>
-            </div>
+        <div class="status-container">
+            <p class="status-livro">Disponível</p>
+            <button class="btn-ver-livro">Ver livro</button>
         </div>
     </div>
-    </body>
-
-</html>
+</div>
