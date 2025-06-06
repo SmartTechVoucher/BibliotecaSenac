d5b2e41cc2d511a5eb23bb145fbf04de49500ae2
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-06-06 17:05:27 -0400

    correcoes component de card

--- projeto/index.php
+++ projeto/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bem-vindo á Biblioteca SENAC HUB ACADEMY!</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="./public/css/usuario/tela-inicial.css">
 </head>
 
--- projeto/public/components/card/card-componente.php
+++ projeto/public/components/card/card-componente.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../../../public/css/components/card-livros.css">
-
-    <div class="card-livro">
+<div class="card-livro">
         <img
             src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1714763387i/212703311.jpg"
             alt="Capa do livro"
             class="capa-livro">
 
         <div class="container-info">
+            <h3 class="titulo-livro">Guia do Mestre - D&D</h3>
             <div>
-                <h3 class="titulo-livro">Guia do Mestre - D&D</h3>
                 <p class="autor-livro">Autor:</p>
                 <p class="autor-nome">(Nome do autor)</p>
             </div>
@@ -26,6 +17,3 @@
             </div>
         </div>
     </div>
-    </body>
-
-</html>
