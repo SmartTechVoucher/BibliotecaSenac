27563c8cfe7f4868783cc1edc8ee778b00e2af9a
vik-cae <vitoriacaetano080623@gmail.com>
2025-07-02 16:45:39 -0400

    implementado tela de desenvolvedores

---
+++ projeto/src/views/usuario/desenvolvedores.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../public/css/usuario/devs.css">
+
+</head>
+<body>
+    <?php
+        include "../../../public/components/header/header.php"
+    ?>
+    <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@400;600;700&display=swap" rel="stylesheet">
+    <div class="logo_dev">
+        <p>DESENVOLVEDORES</p>
+        <h1>DE SISTEMA</h1>
+    </div>   
+
+    <div class="foto_dev">
+        <img src="" alt="">
+    </div>
+    
+</body>
+</html>
