603a20dc86c2b32ce5b625d0f0821469e853c85b
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-07-03 17:04:42 -0400

    att component de card

--- projeto/public/components/usuario/card/card-componente.php
+++ projeto/public/components/usuario/card/card-componente.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../../../css/components/usuario/card-livros.css">
+    <link rel="stylesheet" href="../../../css/components/usuario/card2.css">
 
 </head>
 
---
+++ projeto/public/components/usuario/card/card2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../css/components/usuario/card2.css">
+
+</head>
+
+
+
+<div class="card-livro">
+  <img src="capa_exemplo.jpg" class="capa-livro" alt="Capa do Livro">
+  <div class="conteudo-card">
+    <h2 class="titulo-livro">Guia do Mestre - D&D</h2>
+    <p class="autor-livro">Autor: Fulano de Tal</p>
+    <p class="status-livro disponivel">Disponível</p>
+    <button class="btn-reservar">Reservar</button>
+  </div>
+</div>
+
+
+</html>
