2ff64bd097fbade0df3305e020e034b4e2ae9b4f
vik-cae <174654321+vik-cae@users.noreply.github.com>
2025-07-03 15:58:22 -0300

    subindo css dos cards_desv

--- projeto/src/views/usuario/desenvolvedores.php
+++ projeto/src/views/usuario/desenvolvedores.php
@@ -1,25 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Desenvolvedores</title>
     <link rel="stylesheet" href="../../../public/css/usuario/devs.css">
 
 </head>
+
 <body>
     <?php
-        include "../../../public/components/header/header.php"
+    include "../../../public/components/header/header.php"
     ?>
-    <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@400;600;700&display=swap" rel="stylesheet">
-    <div class="logo_dev">
-        <p>DESENVOLVEDORES</p>
-        <h1>DE SISTEMA</h1>
-    </div>   
+    <main id="main-devs">
+        <section>
+            <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@400;600;700&display=swap" rel="stylesheet">
+            <div class="logo_dev">
+                <p>DESENVOLVEDORES</p>
+                <h1>DE SISTEMA</h1>
+            </div>
+        </section>
 
-    <div class="foto_dev">
-        <img src="" alt="">
-    </div>
-    
+        <section class="cards-grid">
+            <?php for ($i = 0; $i < 10; $i++): ?>
+                <?php include "../../../public/components/card_devs/card_devs.php"; ?>
+            <?php endfor; ?>
+        </section>
+    </main>
+
 </body>
+
 </html>
