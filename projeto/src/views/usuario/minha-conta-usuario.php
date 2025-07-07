8e0eac68b789e30a002b16a994eed129ba221198
Gantt-sucessor <smarttechvoucher@gmail.com>
2025-07-07 10:59:18 -0400

    Terminando Responsividade da tela minha-conta-usuario

--- projeto/src/views/usuario/minha-conta-usuario.php
+++ projeto/src/views/usuario/minha-conta-usuario.php
@@ -24,57 +24,49 @@ require(__DIR__ . '/../../../config/constantes.php');
       </div>
 
       <div class="perfil-content">
-        <!-- Dados da pessoa associada -->
-        <div class="section-dados-pessoa">
-          <label>*Nome<label>
-          <div class="form-group">
-            <input type="text" value="Gabriel Arruda da Costa" class="input-field" readonly>
+        <!-- Primeira linha: Nome, Data de Nascimento, Apelido e Botão -->
+        <div class="primeira-linha">
+          <div class="campo-nome">
+            <label>*Nome</label>
+            <input type="text" value="Gabriel Arruda" class="input-field" readonly>
+          </div>
+
+          <div class="campo-nascimento">
+            <label>*Data de Nascimento:</label>
+            <input type="text" value="04/09/2003" class="input-field" readonly>
+          </div>
+
+          <div class="campo-apelido">
+            <label>Apelido:</label>
+            <input type="text" value="ThigsDelas" class="input-field" readonly>
+          </div>
+
+          <div class="campo-botao">
+            <label>&nbsp;</label>
+            <button class="btn-editar" type="button">Editar apelido</button>
           </div>
         </div>
 
-        <!-- Linha com nascimento, apelido e botão -->
-        <div class="section-row align-apelido">
-          <div class="section-nascimento">
-            <label>*Data de Nascimento:<label>
-            <div class="form-group">
-              <input type="text" value="04/09/2003" class="input-field" readonly>
-            </div>
-          </div>
-
-          <div class="section-apelido">
-            <label>Apelido:<label>
-            <div class="form-group">
-              <input type="text" value="ThigsDelas" class="input-field" readonly>
-            </div>
-          </div>
-
-          <div class="section-editar">
-            <h2>&nbsp;</h2>
-            <div class="form-group">
-              <button class="btn-editar" type="button">Editar apelido</button>
-            </div>
-          </div>
-        </div>
-
-        <!-- Dados do usuário -->
-        <div class="section-usuario">
+        <!-- Título da seção -->
+        <div class="section-title">
           <h2>*Dados do usuário</h2>
+        </div>
 
-          <div class="form-row">
-            <div class="form-group1">
-              <label>E-mail</label>
-              <input type="email" value="amojava@gmail.com" class="input-field" readonly>
-            </div>
+        <!-- Segunda linha: E-mail, Senha e Criado em -->
+        <div class="segunda-linha">
+          <div class="campo-email">
+            <label>E-mail</label>
+            <input type="email" value="amojava@gmail.com" class="input-field" readonly>
+          </div>
 
-            <div class="form-group2">
-              <label>Senha</label>
-              <input type="password" value="************" class="input-field" readonly>
-            </div>
+          <div class="campo-senha">
+            <label>Senha</label>
+            <input type="password" value="************" class="input-field" readonly>
+          </div>
 
-            <div class="form-group3">
-              <label>Criado em:</label>
-              <input type="text" value="13/05/2023" class="input-field" readonly>
-            </div>
+          <div class="campo-criado">
+            <label>Criado em:</label>
+            <input type="text" value="13/05/2023" class="input-field" readonly>
           </div>
         </div>
       </div>
@@ -82,6 +74,8 @@ require(__DIR__ . '/../../../config/constantes.php');
   </main>
 
   <?php include "../../../public/components/usuario/footer/footer.php"; ?>
+  
+  <script src="<?php echo $URLBASE ?>/public/js/usuario/editar-apelido.js"></script>
 </body>
 
 </html>
