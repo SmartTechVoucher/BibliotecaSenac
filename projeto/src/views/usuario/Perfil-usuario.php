aa014d1aef46a48a710c5d1d0712f8fa90583bc1
Matheus612-coder <matheusserpa2000@hotmail.com>
2025-07-28 15:58:41 -0400

    Title e lang de 'Perfil-usuario.php consertados.'

--- projeto/src/views/usuario/Perfil-usuario.php
+++ projeto/src/views/usuario/Perfil-usuario.php
@@ -5,12 +5,12 @@ require(__DIR__ . '/../../../config/constantes.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Perfil do usuário</title>
   <link rel="stylesheet" href="<?php echo $URLBASE ?>/public/css/usuario/perfil-usuario.css">
   <link rel="stylesheet" href="<?php echo $URLBASE ?>/public/css/components/modal.css">
   <link rel="stylesheet" href="<?php echo $URLBASE ?>/public/css/components/voltar.css">
