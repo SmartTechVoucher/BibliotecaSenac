e69df7015bf291e74b5bfbdcaa5f1291b31d0582
Vitor H. <74419765+vitocrack@users.noreply.github.com>
2025-08-15 17:01:07 -0400

    -Adicionado mais parametros para a função do input-admin; -Adicionado inputs novos na tela de cadastro de usuario.

--- projeto/public/components/admin/input/input-admin.php
+++ projeto/public/components/admin/input/input-admin.php
@@ -1,7 +1,49 @@
 
 <body>
     <?php
-function InputAdmin($largura, $placeholder = "", $name="", $id="input-admin",$readonly= false, $tipo ="text") {
+function InputAdmin($largura, $placeholder = "", $name="", $id="input-admin",$readonly= false, $tipo ="text", $required= false, $valor = "") {
+    static $css_adicionado = false;
+
+    // Se o CSS ainda não foi adicionado, crie o bloco <style>
+    if (!$css_adicionado) {
+        $css = '
+<style>
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+
+.input-admin {
+    width: 200px;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    background-color: #ffffff;
+    color: #495057;
+    font-size: 1em;
+    box-sizing: border-box;
+    transition: all 0.1s ease-in-out;
+}
+
+
+.input-admin:read-only {
+    background-color: #e6e6e6;
+}
+
+
+.input-admin:focus {
+    border-color: dodgerblue; 
+    box-shadow: 0 0 5px dodgerblue; 
+    transform: scale(1.02); 
+}
+</style>';
+        echo $css;
+        $css_adicionado = true;
+    }
+    
     $html = '<input class="input-admin" ';
     $html .='type="' . htmlspecialchars($tipo) . '" ';
 
@@ -9,11 +51,16 @@ function InputAdmin($largura, $placeholder = "", $name="", $id="input-admin",$re
     $html .= 'name="' . htmlspecialchars($name) . '" ';
     
     $html .= 'placeholder="' . htmlspecialchars($placeholder) . '" ';
+
+    $html .= 'value="' . htmlspecialchars($valor) . '" ';
     
     $html .= 'style="width:' . htmlspecialchars($largura) . '%;" ';
     
     if ($readonly) {
         $html .= 'readonly ';
+    }
+    if($required){
+        $html .= 'required ';
     }
     
     $html .= '>';
--- projeto/src/views/admin/telaDeCadastroDeUsuarios.php
+++ projeto/src/views/admin/telaDeCadastroDeUsuarios.php
@@ -11,7 +11,7 @@
   <link rel="stylesheet" href="<?php echo $URLBASE ?>/public/css/components/usuario/footer.css">
   <link rel="stylesheet" href="<?php echo $URLBASE ?>/public/css/components/usuario/modal.css">
   <link rel="stylesheet" href="../../../public/css/admin/telaDeCadastroDeUsuarios.css">
-  <link rel="stylesheet" href="<?php echo $URLBASE ?>/public/css/components/admin/input-admin.css">
+  
   <?php include "../../../public/components/admin/input/input-admin.php"; ?>
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -35,7 +35,7 @@
   include "../../../public/components/admin/header/header-admin.php";
   ?>
 
-  <main>
+<main>
     <div class="container-main">
       <form id="cadastro-form" action="" method="">
         <fieldset class="form-section">
@@ -43,78 +43,128 @@
           <div id="foto-perfil-container">
             <img id="foto-perfil" src="<?php echo $URLBASE ?>/public/assets/img/NullUser.jpg" alt="">
             <?php
-            InputAdmin(15, null, "foto-user", tipo:"file");
+            InputAdmin(15, null, "foto-user", tipo: "file");
             ?>
           </div>
           <div class="form-row">
             <div class="form-group">
               <label for="nome">Nome Completo</label>
               <?php
-              InputAdmin("text", 100, null, "nome");
+              InputAdmin(largura: 100, name: "nome", id: "nome", tipo: "text", required: true)
               ?>
             </div>
             <div class="form-group">
               <label for="cpf">CPF</label>
               <?php
-              InputAdmin("text", 100, null, "cpf");
+              InputAdmin(largura: 100, name: "cpf", id: "cpf", tipo: "text", required: true)
               ?>
             </div>
-            <div class="form-row">
-              <div class="form-group">
-                <label for="email">E-mail</label>
-                <?php
-                InputAdmin("email", 100, null, "email");
-                ?>
-              </div>
-              <div class="form-group">
-                <label for="data_nascimento">Data de Nascimento</label>
-                <?php
-                InputAdmin("date", 100, null, "data_nascimento");
-                ?>
-              </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group">
+              <label for="email">E-mail</label>
+              <?php
+              InputAdmin(largura: 100, name: "email", id: "email", tipo: "email");
+              ?>
             </div>
-
+            <div class="form-group">
+              <label for="data_nascimento">Data de Nascimento</label>
+              <?php
+              InputAdmin(largura: 100, name: "data_nascimento", id: "data_nascimento", tipo: "date", required: true)
+              ?>
+            </div>
+          </div>
+          <div class="form-row">
             <div class="form-group">
               <label for="telefone">Telefone</label>
               <?php
-              InputAdmin("tel", 100, "(99) 99999-9999", "telefone");
+              InputAdmin(largura: 100, placeholder: "(99) 99999-9999", name: "telefone", id: "telefone", tipo: "tel")
               ?>
             </div>
-
             <div class="form-group">
               <label for="endereco">Endereço Completo</label>
               <?php
-              InputAdmin(tipo: "text", largura: 100, placeholder: "Ex: Rua das Flores, 123, Centro", name: "endereco", id: "endereco");
+              InputAdmin(largura: 100, placeholder: "Ex: Rua das Flores, 123, Centro", name: "endereco", id: "endereco", tipo: "text");
               ?>
             </div>
-
-            <div class="form-row">
-              <div class="form-group">
-                <label for="nome_social">Nome Social</label>
-                <?php
-              InputAdmin(tipo: "text", largura: 100, placeholder: "Ex: João", name: "nome_social", id: "nome_social");
+          </div>
+          <div class="form-row">
+            <div class="form-group">
+              <label for="nome_social">Nome Social</label>
+              <?php
+              InputAdmin(largura: 100, placeholder: "Ex: João", name: "nome_social", id: "nome_social", tipo: "text");
               ?>
-              </div>
-              <div class="form-group">
-                <label for="genero">Gênero</label>
-                <select id="genero" name="genero">
-                  <option value="">Selecione</option>
-                  <option value="masculino">Masculino</option>
-                  <option value="feminino">Feminino</option>
-                  <option value="nao_binario">Não Binário</option>
-                  <option value="outros">Outros</option>
-                  <option value="nao_informar">Prefiro não informar</option>
-                </select>
-              </div>
+            </div>
+            <div class="form-group">
+              <label for="genero">Gênero</label>
+              <select id="genero" name="genero" class="input-admin">
+                <option value="">Selecione</option>
+                <option value="masculino">Masculino</option>
+                <option value="feminino">Feminino</option>
+                <option value="nao_binario">Não Binário</option>
+                <option value="outros">Outros</option>
+                <option value="nao_informar">Prefiro não informar</option>
+              </select>
             </div>
           </div>
         </fieldset>
+        <fieldset class="form-section">
+          <legend>Informações Acadêmicas</legend>
+          <div class="form-row">
+            <div class="form-group">
+              <label for="matricula">Nº de Matrícula</label>
+              <?php
+              InputAdmin(largura: 100, name: "matricula", id: "matricula", tipo: "text")
+              ?>
+            </div>
+            <div class="form-group">
+              <label for="categoria">Categoria</label>
+              <select id="categoria" name="categoria" class="input-admin" required>
+                <option value="">Selecione</option>
+                <option value="graduacao">Aluno</option>
+                <option value="pos">Docente</option>
+                <option value="extensao">Bibliotecário</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="unidade_senac">Unidade</label>
+              <select id="unidade_senac" name="unidade_senac" class="input-admin" required>
+                <option value="">Selecione</option>
+                <option value="senac_hub">Senac Hub Academy</option>
+                <option value="senac_dou">Senac Dourados</option>
+                <option value="senac_tres">Senac Três Lagoas</option>
+              </select>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group">
+              <label for="curso">Curso</label>
+              <?php
+              InputAdmin(largura: 100, name: "curso", id: "curso", tipo: "text")
+              ?>
+            </div>
+            <div class="form-group">
+              <label for="turma">Turma</label>
+              <?php
+              InputAdmin(largura: 100, name: "turma", id: "turma", tipo: "text")
+              ?>
+            </div>
+            <div class="form-group">
+              <label for="data_fim_curso">Data de Término do Curso</label>
+              <?php
+              InputAdmin(largura: 100, name: "data_fim_curso", id: "data_fim_curs", tipo: "date")
+              ?>
+            </div>
+          </div>
 
-
+        </fieldset>
+        <div class="botao-container">
+                <button type="submit">Salvar Usuário</button>
+                <button type="button" class="botao-cancelar">Cancelar</button>
+            </div>
       </form>
     </div>
-  </main>
-
+</main>
   <!--Rodapé--> <!--Rodapé--> <!--Rodapé--> <!--Rodapé--> <!--Rodapé--> <!--Rodapé--> <!--Rodapé--> <!--Rodapé--> <!--Rodapé--> <!--Rodapé--> <!--Rodapé--> <!--Rodapé-->
 
 
