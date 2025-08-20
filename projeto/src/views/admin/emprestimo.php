e6b676bc5d687c250dfd1ae464ecb8a4a4319f2c
Matheus612-coder <matheusserpa2000@hotmail.com>
2025-08-20 17:09:22 -0400

    Algumas melhorias feitas em questão de responsividade.

--- projeto/src/views/admin/emprestimo.php
+++ projeto/src/views/admin/emprestimo.php
@@ -19,104 +19,106 @@
 </head>
 
 <body>
+    
     <?php
         include "../../../public/components/admin/header/header-admin.php"
     ?>
     
-    <!-- <main> -->
         <div class="quadradoBranco">
-
-            <!-- pesquisas de usuario e livro -->
-            <div class="search-container">
-                <!-- pesquisar usuario -->
-                <div class="searchUser">
-                    <h3>Usuário:</h3>
-                    <form class="searchInput">
-                        <input type="text" name="" id="searchBar" placeholder="Insira nome, CPF ou nº de matrícula">
-                        <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_Usuario.png" alt="">
-                    </form> 
-                </div>
-                
-                <!-- dados do usuario -->
-                <div class="usuarioCadastrado">
                     
-                    <img src="<?php echo $URLBASE?>/public/assets/img/NullUser.jpg" alt="">
-                   
-                    <span id="nameUser"><b>Nome:</b> Carlos Terrel </span>
-                    <br>
-                   
-                    <span><b>N° de matrícula:</b> 31182092025</span>
-                    <span><b>Perfil de acesso:</b> Comum <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_PerfilAcesso.png" alt=""></span>
-                    <br>
-                   
-                    <span><b>Email:</b> iammusic@gmail.com</span>
-                    <span><b>Telefone:</b> 6740028922</span>
-                   
-                </div>
-                
-                <!-- pesquisar livro -->
-                <div class="searchBook">
-                    <h3>Código do livro:</h3>
-                    <form class="searchInput">
-                        <input type="text" name="" id="searchBar" placeholder="Insira código de exemplar">
-                        <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_CodigoDoLivro.png" alt="">
-                    </form>
-                </div>
-
+            <div class="h3">
+                <h3>Usuário:</h3>
             </div>
+                    
+            <form class="searchInput">
+                <input type="text" name="" class="searchBar" placeholder="Insira nome, CPF ou nº de matrícula">
+                <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_Usuario.png" alt="">
+            </form> 
+                
+            <!-- dados do usuario -->
+           <div class="usuarioCadastrado">
+                    
+            <img src="<?php echo $URLBASE?>/public/assets/img/NullUser.jpg" alt="" id="imagem">
+                   
+            <span class="nomeUsuario" id="nome"><b>Nome:</b> Carlos Terrel </span>
+                   
+                   
+            <span class="nomeUsuario2" id="numero"><b>N° de matrícula:</b> 31182092025</span>
+            <span class="nomeUsuario2" id="perfil"><b>Perfil de acesso:</b> Comum</span>
+                    
+            <span class="nomeUsuario" id="email"><b>Email:</b> iammusic@gmail.com</span>
+            <span class="nomeUsuario" id= "telefone"><b>Telefone:</b> 6740028922</span>
+                   
+        </div>
+                
+        <!-- pesquisar livro -->
+        <div class="searchBook">
+            <h3>Código do livro:</h3>
+            
+            <form class="searchInput">
+                <input type="text" name="" class="searchBar" placeholder="Insira código de exemplar">
+                <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_CodigoDoLivro.png" alt="">
+            </form>
+        </div>
 
             <!-- emprestimos já cadastrados no nome do usuario -->
             <div class="livrosEmprestados">
+                
                 <div class="tabela-header">
                     <div class="div1">Capa</div>
                     <div class="div2">Título</div>
-                    <div class="div3">Exemplar</div>
+                    <!-- <div class="div3">Exemplar</div>
                     <div class="div4">Data empres.</div>
-                    <div class="div5">Data devo.</div>
+                    <div class="div5">Data devo.</div> -->
                 </div>
 
                 <div class="tabela-footer"> 
                     <div class="book-grid">
                         <img class="grid1" src="<?php echo $URLBASE?>/public/assets/img/livroCapa.jpg"></img>
                         <div class="grid2"><a href="" url="">A Gaia Ciência</a></div>
-                        <div class="grid3">618.92 T157e</div>
+                        <!-- <div class="grid3">618.92 T157e</div>
                         <div class="grid4">18/03/2025</div>
-                        <div class="grid5">21/03/2025</div>
-                        <div class="grid6">
+                        <div class="grid5">21/03/2025</div> -->
+                        <!-- <div class="grid6">
                             <button id="grid-button">Devolver <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_Devolver.png" alt=""></button>
                             <button id="grid-button">Renovar <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_Renovar.png" alt=""></button>
-                        </div>
+                        </div> -->
                     </div>
+                    
                     <div class="book-grid">
                         <img class="grid1" src="<?php echo $URLBASE?>/public/assets/img/livroCapa.jpg"></img>
                         <div class="grid2"><a href="" url="">A Gaia Ciência</a></div>
-                        <div class="grid3">618.92 T157e</div>
+                        <!-- <div class="grid3">618.92 T157e</div>
                         <div class="grid4">18/03/2025</div>
                         <div class="grid5">21/03/2025</div>
                         <div class="grid6">
                             <button id="grid-button">Devolver <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_Devolver.png" alt=""></button>
                             <button id="grid-button">Renovar <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_Renovar.png" alt=""></button>
-                        </div>
-                    </div><div class="book-grid">
+                        </div> -->
+                    </div>
+                    
+                    <div class="book-grid">
                         <img class="grid1" src="<?php echo $URLBASE?>/public/assets/img/livroCapa.jpg"></img>
                         <div class="grid2"><a href="" url="">A Gaia Ciência</a></div>
-                        <div class="grid3">618.92 T157e</div>
+                        <!-- <div class="grid3">618.92 T157e</div>
                         <div class="grid4">18/03/2025</div>
                         <div class="grid5">21/03/2025</div>
                         <div class="grid6">
                             <button id="grid-button">Devolver <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_Devolver.png" alt=""></button>
                             <button id="grid-button">Renovar <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_Renovar.png" alt=""></button>
-                        </div>
-                    </div><div class="book-grid">
+                        </div> -->
+                    </div>
+                    
+                    <div class="book-grid">
                         <img class="grid1" src="<?php echo $URLBASE?>/public/assets/img/livroCapa.jpg"></img>
                         <div class="grid2"><a href="" url="">A Gaia Ciência</a></div>
-                        <div class="grid3">618.92 T157e</div>
+                        <!-- <div class="grid3">618.92 T157e</div>
                         <div class="grid4">18/03/2025</div>
                         <div class="grid5">21/03/2025</div>
                         <div class="grid6">
                                 <button type="submit" id="grid-button">Devolver <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_Devolver.png" alt=""></button>
                                 <button type="submit" id="grid-button">Renovar <img src="<?php echo $URLBASE?>/public/assets/icons/Icone_Renovar.png" alt=""></button>
-                        </div>
+                        </div> -->
                     </div>
                 </div>                
                 
