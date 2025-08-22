8be6c5f9e81dfc872f528ec36c005f8bbd17d29c
Vitor H. <74419765+vitocrack@users.noreply.github.com>
2025-08-22 17:08:01 -0400

    melhorias

--- projeto/src/views/admin/telaDeCadastroDeLivros.php
+++ projeto/src/views/admin/telaDeCadastroDeLivros.php
@@ -15,15 +15,15 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,50;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,50;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,50;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,50;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,50;0,300;0,400;0,500;0,700;0,900;1,50;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montaga&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montaga&family=Poppins:ital,wght@0,50;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,50;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,50;0,300;0,400;0,500;0,700;0,900;1,50;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
 </head>
 
@@ -39,47 +39,71 @@
             <fieldset class="form-section">
                 <legend>Cadastro de livro</legend>
                 <div class="form-coluna">
-                    <label for="titulo-livro">Titulo</label>
+                    <div class="form-grupo">
+                        <label for="titulo-livro">Titulo</label>
                     <?php
                     InputAdmin(largura: 100, name: "titulo-livro", id: "titulo-livro", required: true)
                     ?>
-                    <label for="autor-livro">Autor</label>
+                    </div>
+                    <div class="form-grupo">
+                       <label for="autor-livro">Autor</label>
                     <?php
                     InputAdmin(largura: 100, name: "autor-livro", id: "autor-livro", required: true)
-                    ?>
-                    <label for="titulo-livro">Editora</label>
+                    ?> 
+                    </div>
+                    <div class="form-grupo">
+                        <label for="titulo-livro">Editora</label>
                     <?php
                     InputAdmin(largura: 100, name: "titulo-livro", id: "titulo-livro", required: true)
                     ?>
-                    <label for="codigo-livro">Código do livro</label>
+                    </div>
+                    <div class="form-grupo">
+                        <label for="codigo-livro">Código do livro</label>
                     <?php
                     InputAdmin(largura: 100, name: "codigo-livro", id: "codigo-livro", required: true)
                     ?>
-                    <label for="idioma-livro">Idioma</label>
+                    </div>
+                    <div class="form-grupo">
+                        <label for="idioma-livro">Idioma</label>
                     <?php
                     InputAdmin(largura: 100, name: "idioma-livro", id: "idioma-livro")
                     ?>
-                    <label for="categoria-livro">Categoria</label>
+                    </div>
+                    <div class="form-grupo">
+                        <label for="categoria-livro">Categoria</label>
                     <?php
                     InputAdmin(largura: 100, name: "categoria-livro", id: "categoria-livro")
                     ?>
-                    <label for="area-livro">Area</label>
+                    </div>
+                    <div class="form-grupo">
+                        <label for="area-livro">Area</label>
                     <?php
                     InputAdmin(largura: 100, name: "area-livro", id: "area-livro")
                     ?>
-                    <label for="publicacao-livro">Ano</label>
+                    </div>
+                    <div class="form-grupo">
+                        <label for="publicacao-livro">Ano</label>
                     <?php
                     InputAdmin(largura: 100, name: "publicacao-livro", id: "publicacao-livro", tipo: "date")
                     ?>
-                    <label for="capa-livro">Capa</label>
+                    </div>
+                    <div class="form-grupo">
+                       <label for="capa-livro">Capa</label>
                     <?php
                     InputAdmin(largura: 100, name: "capa-livro", id: "capa-livro", tipo: "file")
-                    ?>
+                    ?> 
+                    </div>
+                    
 
                 </div>
                 <div class="form-coluna">
-                    <textarea name="resumo-livro" id="resumo-livro" cols="30" rows="10"></textarea>
+                <div class="form-grupo">
+                     <textarea name="resumo-livro" id="resumo-livro" cols="30" rows="10"></textarea>
+                </div>
+                <div class="form-grupo">
                     <textarea name="notas-livro" id="notas-livro" cols="30" rows="10"></textarea>
+                </div>
+                <div class="form-grupo">
                     <label for="tipo-documento">Tipo de documento</label>
                     <select id="tipo-documento" name="tipo-documento" class="input-admin" required>
                         <option value="">Selecione</option>
@@ -87,6 +111,8 @@
                         <option value="tipo-documento-ebook">eBook</option>
                         <option value="tipo-documento-revista">Revista</option>
                     </select>
+                </div>
+                    
 
             </fieldset>
 
