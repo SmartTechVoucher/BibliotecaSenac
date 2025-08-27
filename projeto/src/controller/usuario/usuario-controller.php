b61aa9beff8a4a278f0c2a95d9674b7759ec67cc
SmartTechVoucher <smarttechvoucher@gmail.com>
2025-08-27 19:50:22 -0400

    arrumando o usuario-controller

--- projeto/src/controller/usuario/usuario-controller.php
+++ projeto/src/controller/usuario/usuario-controller.php
@@ -9,16 +9,35 @@ class UsuarioController{
         $this->db = new Database();
     }
 
-    public function criarUsuario($nome, $senha, $nome_social, $email){
+    public function criarUsuario($nome, $nome_social, $cpf, $email, $data_nascimento, $telefone, $rua, $bairro, 
+    $numero_matricula, $id_categoria_curso, $id_curso, $data_inicio, $data_fim, $genero, $senha){
         try {
             $conn = $this->db->Connect();
-            $sql = "INSERT INTO usuario (nome, senha, email) VALUES (:nome, :senha, :email)";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':nome', $nome);
-            $stmt->bindParam(':senha', password_hash($senha, PASSWORD_BCRYPT));
-            $stmt->bindParam(':email', $email);
-            $stmt->execute();
-            return true;
+            $sql = "INSERT INTO usuarios (nome, nome_social, cpf, email, data_nascimento, telefone, rua, bairro, 
+            numero_matricula, id_categoria_curso, id_curso, data_inicio, data_fim, genero, senha) VALUES (:nome, :nome_social, :cpf, :email, :data_nascimento, :telefone, :rua, :bairro, 
+            :numero_matricula, :id_categoria_curso, :id_curso, :data_inicio, :data_fim, :genero, :senha)";
+            $db = $conn->prepare($sql);
+            $db->bindParam(':nome', $nome);
+            $db->bindParam(':nome_social', $nome_social);
+            $db->bindParam(':cpf', $cpf);
+            $db->bindParam(':data_nascimento', $data_nascimento);
+            $db->bindParam(':telefone', $telefone);
+            $db->bindParam(':rua', $rua);
+            $db->bindParam(':bairro', $bairro);
+            $db->bindParam(':numero_matricula', $numero_matricula);
+            $db->bindParam(':id_categoria_curso', $id_categoria_curso);
+            $db->bindParam(':id_curso', $id_curso);
+            $db->bindParam(':data_inicio', $data_inicio);
+            $db->bindParam(':data_fim', $data_fim);
+            $db->bindParam(':genero', $genero);
+            $db->bindParam(':senha', password_hash($senha, PASSWORD_BCRYPT));
+            $db->bindParam(':email', $email);
+
+            if($db->execute()){
+                return True;
+            }else{
+                return False;
+            }
         } catch (PDOException $e) {
             // Log error or handle it as needed
             return false;
